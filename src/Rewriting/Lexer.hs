27ecccfbf4d4bb5b47e3ae46f6c21037dcad23f5
Geoff Hulette <ghulette@gmail.com>
2010-12-05 14:15:39 -0800

    Cleanup, warnings, factor out lexer

---
+++ src/Rewriting/Lexer.hs
@@ -0,0 +1,42 @@
+module Rewriting.Lexer where
+
+import Text.ParserCombinators.Parsec
+import qualified Text.ParserCombinators.Parsec.Token as Tok
+import Text.ParserCombinators.Parsec.Language (haskellStyle)
+
+-- Tokens
+
+lexer :: Tok.TokenParser ()
+lexer = Tok.makeTokenParser style
+  where ops = [";","|","?","~","->","="]
+        ids = ["T","F"]
+        style = haskellStyle {Tok.reservedOpNames = ops,
+                              Tok.reservedNames = ids}
+
+lexeme :: Parser a -> Parser a
+lexeme = Tok.lexeme lexer
+
+parens :: Parser a -> Parser a
+parens = Tok.parens lexer
+
+comma :: Parser ()
+comma = Tok.comma lexer >> return ()
+
+brackets :: Parser a -> Parser a
+brackets = Tok.brackets lexer
+
+reserved :: String -> Parser ()
+reserved = Tok.reserved lexer
+
+reservedOp :: String -> Parser ()
+reservedOp = Tok.reservedOp lexer
+
+identifier :: Parser String
+identifier = Tok.identifier lexer
+
+allOf :: Parser a -> Parser a
+allOf p = do
+  Tok.whiteSpace lexer
+  r <- p
+  eof
+  return r
--- src/Rewriting/Parser.hs
+++ src/Rewriting/Parser.hs
@@ -1,32 +1,11 @@
 module Rewriting.Parser where
 
-import Rewriting.Term
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Expr
-import qualified Text.ParserCombinators.Parsec.Token as Tok
-import Text.ParserCombinators.Parsec.Language (haskellStyle)
+import Rewriting.Term
+import Rewriting.Lexer
 
-lexer :: Tok.TokenParser ()
-lexer = Tok.makeTokenParser style
-  where ops = [";","|","?","~","->","="]
-        ids = ["T","F"]
-        style = haskellStyle {Tok.reservedOpNames = ops,
-                              Tok.reservedNames = ids}
-
-whiteSpace = Tok.whiteSpace lexer
-lexeme     = Tok.lexeme lexer
-symbol     = Tok.symbol lexer
-natural    = Tok.natural lexer
-parens     = Tok.parens lexer
-brackets   = Tok.brackets lexer
-semi       = Tok.semi lexer
-comma      = Tok.comma lexer
-identifier = Tok.identifier lexer
-reserved   = Tok.reserved lexer
-reservedOp = Tok.reservedOp lexer
-
-
--- Parsing terms and rule literals
+-- Terms and rule literals
 
 variable :: Parser Term
 variable = lexeme $ do
@@ -36,12 +15,12 @@ variable = lexeme $ do
 
 constant :: Parser Term
 constant = do
-  x <- constId
+  x <- lexeme constId
   ts <- option [] termList
   return $ Const x ts
-  where constId = lexeme $ do x <- lower
-                              xs <- many alphaNum
-                              return (x:xs)
+  where constId = do x <- lower
+                     xs <- many alphaNum
+                     return (x:xs)
 
 term :: Parser Term
 term = variable <|> constant <?> "term"
@@ -57,7 +36,7 @@ rule = do
   return $ Rule t1 t2
 
 
--- Parsing rule expressions
+-- Expressions
 
 data RuleExpr = RuleVar String
               | RuleLit Rule
@@ -72,9 +51,12 @@ data RuleExpr = RuleVar String
               -- | BranchSome RuleExpr
               deriving (Eq,Show)
 
+ruleId :: Parser String
+ruleId = identifier
+
 ruleVar :: Parser RuleExpr
 ruleVar = do
-  x <- identifier
+  x <- ruleId
   return (RuleVar x)
 
 ruleLit :: Parser RuleExpr
@@ -108,26 +90,16 @@ ruleExpr = buildExpressionParser table factor
 
 ruleAssign :: Parser (String,RuleExpr)
 ruleAssign = do
-  x <- identifier
+  x <- ruleId
   reservedOp "="
   r <- ruleExpr
   return (x,r)
-
-allOf :: GenParser Char () a -> GenParser Char () a
-allOf p = do
-  whiteSpace
-  r <- p
-  eof
-  return r
 
 
 -- Wrappers
 
 parseRules :: String -> Either ParseError [(String,RuleExpr)]
 parseRules = parse (allOf (many ruleAssign)) "Rules"
-
-parseRule :: String -> Either ParseError Rule
-parseRule = parse (allOf rule) "Rule"
 
 parseTerm :: String -> Either ParseError Term
 parseTerm = parse term "Term"
