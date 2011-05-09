8b265ee77c56d0cbe62a3aecf0f586a6ec118d41
Geoff Hulette <ghulette@gmail.com>
2011-05-09 14:39:24 -0700

    Update

--- src/RuleExpr.hs
+++ src/RuleExpr.hs
@@ -137,6 +137,23 @@ eval (Call x args) defs env t =
       let env' = bindVars env (zip params ss)
       eval e defs env' t
 
+reduce :: Eq a => (a -> a) -> a -> a
+reduce f x = case f x of x' | x' == x -> x'
+                         x' -> reduce f x'
+
+normalize :: RuleExpr -> RuleExpr
+normalize (Seq a (LeftChoice b c)) = LeftChoice (Seq a b) (Seq a c)
+normalize (Seq (LeftChoice a b) c) = LeftChoice (Seq a c) (Seq b c)
+normalize (Test e) = Test (normalize e)
+normalize (Neg e) = Neg (normalize e)
+normalize (Fix x e) = Fix x (normalize e)
+normalize (Path i e) = Path i (normalize e)
+normalize (BranchOne e) = BranchOne (normalize e)
+normalize (BranchAll e) = BranchAll (normalize e)
+normalize (BranchSome e) = BranchSome (normalize e)
+normalize (Congruence es) = Congruence (map normalize es)
+normalize x = x
+
 run :: Id -> Env Proc -> Strategy
 run entry defs t = 
   case Env.lookup entry defs of 
