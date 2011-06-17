9a92925ecb68c79df9d46daa2b003187be5f0320
Geoff Hulette <ghulette@gmail.com>
2011-06-17 16:48:05 -0700

    Update

--- experimental/codegen/src/CodeGen.hs
+++ experimental/codegen/src/CodeGen.hs
@@ -2,36 +2,23 @@ module CodeGen where
 
 import Supply
 
-type Id = String
+data Snip = Snip String | Input Int | Output Int
 
-data Snip = Snip String | In Int | Out Int
-data Code = Code { numInputs :: Int, numOutputs :: Int, snippet :: [Snip] }
+renderSnip :: Snip -> [String] -> [String] -> String
+renderSnip (Snip s) _ _ = return s
+renderSnip (Input i) inputs _ = inputs !! i
+renderSnip (Output i) _ outputs = outputs !! i
+      
+data Code = Code [Snip]
           | Seqn Code Code
-          | Congr [Code]
-          | Branch [Code]
+          | Par Code Code
 
-seqn :: Code -> Code -> Supply Id Code
-seqn c1 c2 | numOutputs c1 /= numInputs c2 = error "seqn"
-seqn c1 c2 = do
-  ids <- supplies (numOutputs c1)
-  let c1snip' = map (rewriteOutput ids) (snippet c1)
-  let c2snip' = map (rewriteInput ids) (snippet c2)
-  let snip' = c1snip' ++ c2snip'
-  return (Code (numInputs c1) (numOutputs c2) snip')
-  
-branch :: [Code] -> Code
-branch = undefined
+data Rendered = Rendered 
+              { code :: String
+              , inputs :: [String]
+              , outputs :: [String]
+              }
 
-congruence :: [Code] -> Code
-congruence = undefined
+render :: Code -> [String] -> [String] -> Supply Rendered
+render = undefined
 
-wrap :: (Code,Code) -> Code -> Code
-wrap = undefined
-
-rewriteInput :: [Id] -> Snip -> Snip
-rewriteInput ids (In i) = Snip (ids !! i)
-rewriteInput _ x = x
-
-rewriteOutput :: [Id] -> Snip -> Snip
-rewriteOutput ids (Out i) = Snip (ids !! i)
-rewriteOutput _ x = x
