d7398acc4836c82f8ea9fc31b4b24d333ce146b7
Geoff Hulette <ghulette@gmail.com>
2011-06-22 17:40:10 -0700

    Basic idea

--- experimental/codegen/src/CodeGen.hs
+++ experimental/codegen/src/CodeGen.hs
@@ -1,24 +1,69 @@
 module CodeGen where
 
+import Control.Monad (guard)
 import Supply
 
-data Snip = Snip String | Input Int | Output Int
+data Type = Int | Float | Double | Ptr Type deriving (Eq,Show)
 
-renderSnip :: Snip -> [String] -> [String] -> String
-renderSnip (Snip s) _ _ = return s
-renderSnip (Input i) inputs _ = inputs !! i
-renderSnip (Output i) _ outputs = outputs !! i
-      
-data Code = Code [Snip]
+type Id = String
+
+type InputType = Type
+type OutputType = Type
+
+data Snip = Snip String | InVar Int | OutVar Int
+
+data Code = Code [InputType] [OutputType] [Snip]
           | Seqn Code Code
           | Par Code Code
 
-data Rendered = Rendered 
-              { code :: String
-              , inputs :: [String]
-              , outputs :: [String]
-              }
+inputTypes :: Code -> [InputType]
+inputTypes (Code ins _ _) = ins
+inputTypes (Seqn c1 _) = inputTypes c1
+inputTypes (Par c1 c2) = inputTypes c1 ++ inputTypes c2
 
-render :: Code -> [String] -> [String] -> Supply Rendered
-render = undefined
+outputTypes :: Code -> [OutputType]
+outputTypes (Code _ outs _) = outs
+outputTypes (Seqn _ c2) = outputTypes c2
+outputTypes (Par c1 c2) = outputTypes c1 ++ outputTypes c2
 
+code :: [InputType] -> [OutputType] -> [Snip] -> Maybe Code
+code inputs outputs snip = do
+  return (Code inputs outputs snip)
+
+seqn :: Code -> Code -> Maybe Code
+seqn c1 c2 = do
+  guard $ outputTypes c1 == inputTypes c2
+  return (Seqn c1 c2)
+
+par :: Code -> Code -> Maybe Code
+par c1 c2 = do
+  return (Par c1 c2)
+
+renderSnip :: [Id] -> [Id] -> Snip -> String
+renderSnip _ _       (Snip s)   = s
+renderSnip inputs _  (InVar i)  = inputs  !! i
+renderSnip _ outputs (OutVar i) = outputs !! i
+
+renderWithIds :: Code -> [Id] -> [Id] -> Supply Id String
+renderWithIds (Code inTypes outTypes xs) inputs outputs = do
+  return $ concatMap (renderSnip inputs outputs) xs
+renderWithIds (Seqn c1 c2) inputs outputs = do
+  xs <- supplies (length (outputTypes c1))
+  t1 <- renderWithIds c1 inputs xs
+  t2 <- renderWithIds c2 xs outputs
+  return $ t1 ++ "\n" ++ t2
+renderWithIds (Par c1 c2) inputs outputs = do
+  let inputs1 = take (length (inputTypes c1)) inputs
+  let inputs2 = drop (length (inputTypes c1)) inputs
+  let outputs1 = take (length (outputTypes c1)) outputs
+  let outputs2 = drop (length (outputTypes c1)) outputs
+  t1 <- renderWithIds c1 inputs1 outputs1
+  t2 <- renderWithIds c2 inputs2 outputs2
+  return $ t1 ++ "\n" ++ t2
+
+render :: Code -> Supply Id (String,[Id],[Id])
+render c = do
+  inputs <- supplies (length (inputTypes c))
+  outputs <- supplies (length (outputTypes c))
+  t <- renderWithIds c inputs outputs
+  return (t,inputs,outputs)
