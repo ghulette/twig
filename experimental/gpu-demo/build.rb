aa02f3b1d071006b491a7bb9883080165581b41d
Geoff Hulette <ghulette@gmail.com>
2011-06-27 17:10:08 -0700

    Added basic JNI skeleton

---
+++ experimental/gpu-demo/build.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+FILES = ["convolution"]
+
+SDK = "/Developer/SDKs/MacOSX10.6.sdk"
+JAVA = File.join(SDK, "System/Library/Frameworks/JavaVM.framework")
+HEADERS = File.join(JAVA, "Headers")
+SRCDIR = "src"
+OBJDIR = "build/obj"
+LIBDIR = "build/lib"
+
+`mkdir -p #{OBJDIR}`
+`mkdir -p #{LIBDIR}`
+FILES.each {|f| print `gcc -I#{HEADERS} -c #{SRCDIR}/#{f}.c -o #{OBJDIR}/#{f}.o`}
+FILES.each {|f| print `gcc -dynamiclib -o #{LIBDIR}/lib#{f}.jnilib #{OBJDIR}/#{f}.o`}
+puts "ok"
--- experimental/gpu-demo/src/Convolution.java
+++ experimental/gpu-demo/src/Convolution.java
@@ -1,5 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Convolution {
+  public native void test();
+  
   public double[] apply(double[] data) {
-    return data;
+    return Arrays.copyOf(data,data.length);
+  }
+  
+  static {
+    // This is not ideal, assumes a very particular directory structure.
+    try {
+      File dir = new File(".");
+      String pwd = dir.getCanonicalPath();
+      System.load(pwd + "/build/lib/libconvolution.jnilib");
+    }
+    catch(IOException ex) {
+      ex.printStackTrace();
+    }
   }
 }
