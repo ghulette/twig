f0db9815803fead94666661f8ae9a5a9db81cfed
Geoff Hulette <ghulette@gmail.com>
2011-10-19 10:45:23 -0700

    Almost working

--- experimental/examples/simple/byhand_wrap.c
+++ experimental/examples/simple/byhand_wrap.c
@@ -4,7 +4,7 @@ void handle_error() {
   exit(1);
 }
 
-PyObject *_wrap_avg(PyObject *self, PyObject *args) {
+static PyObject *wrap_avg(PyObject *self, PyObject *args) {
   int arg1;
   int arg2;
   float result;
@@ -24,11 +24,10 @@ PyObject *_wrap_avg(PyObject *self, PyObject *args) {
   }
   arg2 = (int)PyInt_AsLong(obj1);
   result = avg(arg1,arg2);
-  resultobj = PyFloat_FromDouble((double)result);
-  return resultobj;
+  return Py_BuildValue("f",result);;
 }
 
-PyObject *_wrap_output(PyObject *self, PyObject *args) {
+static PyObject *wrap_output(PyObject *self, PyObject *args) {
   char *arg1;
   char *arg1_buf;
   Py_ssize_t arg1_len;
@@ -46,4 +45,14 @@ PyObject *_wrap_output(PyObject *self, PyObject *args) {
   output(arg1);
   // Should we copy the string back to Python?
   Py_RETURN_NONE;
+}
+
+static PyMethodDef SimpleMethods[] = {
+    {"avg", wrap_avg, METH_VARARGS, "Average."},
+    {"output", wrap_output, METH_VARARGS, "Print out."},
+    {NULL, NULL, 0, NULL}
+};
+
+PyMODINIT_FUNC initsimple(void) {
+    (void)Py_InitModule("simple", SimpleMethods);
 }
