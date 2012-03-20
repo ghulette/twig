b1e897c43cb86063adbaf7a0fcdfddaeb618e62c
Geoff Hulette <ghulette@gmail.com>
2012-03-19 17:43:36 -0700

    Update

--- experimental/multi-lang-demo/c-python-call/call.c
+++ experimental/multi-lang-demo/c-python-call/call.c
@@ -2,23 +2,24 @@
 
 #include "Python.h"
 
-int main() {
+void do_call() {
   PyObject *args,*a,*b,*c;
   PyObject *module,*dict,*func;
   
-  Py_Initialize();
-  PySys_SetPath(".");
   module = PyImport_ImportModule("Test");
   dict = PyModule_GetDict(module);
   func = PyDict_GetItemString(dict,"foo");
   a = PyInt_FromLong(3);
   b = PyInt_FromLong(5);
   args = PyTuple_Pack(2,a,b);
-  if(!PyCallable_Check(func)) {
-    printf("Not callable\n");
-    return -1;
-  }
   c = PyEval_CallObject(func,args);
   printf("result = %ld\n", PyInt_AsLong(c));
+}
+
+int main() {
+  Py_Initialize();
+  PySys_SetPath(".");
+  do_call();
+  Py_Finalize();
   return 0;
 }
---
+++ experimental/multi-lang-demo/target.c
@@ -0,0 +1,34 @@
+// This is what we want to generate.
+
+#include <jansson.h>
+
+PyObject *call_python(char *module, char *f, PyObject *in) {
+  PyObject *module,*dict,*func,*out;
+  
+  module = PyImport_ImportModule(module);
+  dict = PyModule_GetDict(module);
+  func = PyDict_GetItemString(dict,f);
+  out = PyEval_CallObject(func,in);
+  return out;
+}
+
+void gen(PyObject *in) {
+  // [py(address) -> py(json(int,string))]
+  PyObject *x1 = call_python("target","gen1_py",in);
+
+  // This is the problem?
+  // [py(json(int,string)) -> json(int,string)]
+  char *x2 = PyString_AsString(x1); 
+  json_error_t tmp1;
+  json_t *x3 = json_loads(x2, JSON_DECODE_ANY, &tmp1);
+  
+  // [json(int,string) -> (json(int),json(string))]
+  json_t *x4 = json_array_get(x3,0);
+  json_t *x5 = json_array_get(x3,1);
+  
+  // [json(int) -> int]
+  int x6 = json_integer_value(x4);
+
+  // [json(string) -> string]
+  char *x7 = json_string_value(x5);
+}
