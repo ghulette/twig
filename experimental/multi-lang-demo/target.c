d4d01169ed989d55e0dc369dd5a238134db9e7ef
Geoff Hulette <ghulette@gmail.com>
2012-04-08 14:56:31 -0700

    Update

--- experimental/multi-lang-demo/target.c
+++ experimental/multi-lang-demo/target.c
@@ -6,9 +6,9 @@ PyObject *call_python(char *module, char *f, PyObject *in) {
   PyObject *module,*dict,*func,*out;
   
   module = PyImport_ImportModule(module);
-  dict = PyModule_GetDict(module);
-  func = PyDict_GetItemString(dict,f);
-  out  = PyEval_CallObject(func,in);
+  dict   = PyModule_GetDict(module);
+  func   = PyDict_GetItemString(dict,f);
+  out    = PyEval_CallObject(func,in);
   return out;
 }
 
@@ -18,7 +18,7 @@ void gen(PyObject *in) {
 
   // This is the problem?
   // [py(json(int,string)) -> json(int,string)]
-  json_error_t tmp1; // <-- need to use tmps for this or blocks
+  json_error_t tmp1; // <-- need to use tmps for this or scope
   char *x2 = PyString_AsString(x1);
   json_t *x3 = json_loads(x2, JSON_DECODE_ANY, &tmp1);
   
